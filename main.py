d17d34dfdf1a642e99f62e6522677d9acb7162c1
omkar khandalkar <53345307+oskorp@users.noreply.github.com>
2021-06-16 18:41:29 +0530

    Added translation function and search function
    Added translation function which helps user to translate something from one language to another using translate modules and added google and youtube search function 
    So user can search google and youtbe through voice

--- main.py
+++ main.py
@@ -6,28 +6,39 @@ import webbrowser
 import smtplib
 import pyaudio
 import os
-
+from tkinter import *
+from tkinter import ttk
+from ttkthemes import themed_tk as tk
+from translate import Translator
+import pyjokes
+import pyautogui
 
 engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
 
+
+T=Translator(
+from_lang = "English", to_lang = "Hindi"
+)
 
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def screenshot():
+    pic=pyautogui.screenshot()
+    pic.save('c:/User')
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    
-    if hour>=0 and hour<6:
+
+    if hour >= 0 and hour < 6:
         speak("Hey Owl")
-        
-    elif hour>=6 and hour<12:
+
+    elif hour >= 6 and hour < 12:
         speak("Good Morning")
 
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         speak("Good Afternoon")
 
     else:
@@ -38,7 +49,6 @@ def wishMe():
 
 
 def takeCommand():
-
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening....")
@@ -53,7 +63,8 @@ def takeCommand():
         print(e)
         print("Say that again please.....")
         return "None"
-    return  query
+    return query
+
 
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -63,6 +74,10 @@ def sendEmail(to, content):
     server.sendmail('yourgmail@gmail.co', to, content)
     server.close()
 
+def joke():
+    speak(pyjokes.get_jokes(language='en',category='all'))
+
+
 if __name__ == "__main__":
     wishMe()
     while True:
@@ -70,8 +85,8 @@ if __name__ == "__main__":
 
         if 'wikipedia' in query:
             speak('Searching Wikipedia....')
-            query = query.replace('wikipedia','')
-            results = wikipedia.summary(query, sentences=1)
+            query = query.replace('wikipedia', '')
+            results = wikipedia.summary(query, sentences=2)
             speak('According to wikipedia')
             speak(results)
             print(results)
@@ -113,9 +128,58 @@ if __name__ == "__main__":
                 print(e)
                 speak("Sorry sir. I am not able to send this email")
 
+        elif 'shutdown' in query:
+            print("shutting down...")
+            speak("shutting down")
+            quit()
 
-            
-            
+        elif 'translate' in query:
+            query = query.replace('translate', '')
+            translation = T.translate(query)
+            speak(translation)
+            print(translation)
+            speak(translation)
+
+        elif 'joke' in query:
+            joke()
+
+        elif 'screenshot' in query:
+            screenshot()
+
+        elif 'search youtube' in query:
+            speak("Kya dekhna pasand karoge boss")
+            search= takeCommand().lower()
+            speak('Muje ye videos mili')
+            wb.open('https:/www.youtube.com/results/search_query='+search)
+
+        elif 'search google' in query:
+            speak("Kya search karu janab?")
+            search=takeCommand().lower()
+            speak('wahh kya smart hu mein ye lijiye')
+            wb.open('https:/www.google.com/results/search_query='+search)
 
 
 
+
+
+
+
+
+
+'''
+main()
+
+root_1 = tk.ThemedTk()
+root_1.set_theme('radiance')
+root_1.title("Voice assistant")
+root_1.geometry("400x350")
+lbl1=ttk.Label(master=root_1, text="Welcome to voice assistant app\n\n", wraplength=600)
+lbl1.pack()
+but1 = ttk.Button(root_1, text="Run the assistant🤖🔊", command=main)
+but1.config(width=22)
+but1.pack(padx=10, pady=10)
+quit4 = ttk.Button(root_1, text="EXIT", command=root_1.destroy)
+quit4.config(width=22)
+quit4.pack(padx=10, pady=20)
+root_1.mainloop()
+'''
